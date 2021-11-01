85fdddcf2337bb740bd39c276bfe3085f3a41e64
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2021-11-01 10:56:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # https://github.com/messense/aliyundrive-webdav
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
 
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile
@@ -20,6 +21,5 @@ sed -i "s/OpenWrt /${Author} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt 
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/luci-theme-infinityfreedom
 git clone https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git ./package/lean/luci-app-jd-dailybonus
 git clone https://github.com/fw876/helloworld.git ./package/helloworld
 git clone https://github.com/vernesong/OpenClash.git ./package/lean/luci-app-openclash
